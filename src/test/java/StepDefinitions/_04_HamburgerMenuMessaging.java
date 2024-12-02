85b2f6b114cc4d1d72e4a6692d091dd92fd4a7d3
Aziz Gunes <azizgunes@gail.com>
2024-12-03 00:41:30 +0300

    04 HamburgerMenuMessaging feature eklendi.

---
+++ src/test/java/StepDefinitions/_04_HamburgerMenuMessaging.java
@@ -0,0 +1,41 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class _04_HamburgerMenuMessaging {
+    @When("^Click on the Hamburger Menu$")
+    public void clickOnTheHamburgerMenu() {
+    }
+
+    @And("^Click on the Messages$")
+    public void clickOnTheMessages() {
+    }
+
+    @And("^Hover Over in the Messages$")
+    public void hoverOverInTheMessages() {
+    }
+
+    @And("^Click on the Send Message$")
+    public void clickOnTheSendMessage() {
+    }
+
+    @And("^Click on the Inbox$")
+    public void clickOnTheInbox() {
+    }
+
+    @And("^Click on the Outbox$")
+    public void clickOnTheOutbox() {
+    }
+
+    @And("^Click on the Trash$")
+    public void clickOnTheTrash() {
+    }
+
+    @Then("^User should be succesfully$")
+    public void userShouldBeSuccesfully() {
+    }
+
+
+}
